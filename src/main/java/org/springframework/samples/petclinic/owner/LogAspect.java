8c5bbeb2a9b33530d84092baa72d34fa23a2d218
MoonHKLee <mf311110@gmail.com>
2019-05-26 17:41:08 +0900

    백기선 스프링입문 3강 과제 완료

---
+++ src/main/java/org/springframework/samples/petclinic/owner/LogAspect.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAspect  {
+
+
+}
